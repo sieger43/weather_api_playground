c98826ffcdebc4de75b7f619c916c958ff769102
John Berndt <johnwberndt@gmail.com>
2019-11-18 20:59:18 -0500

    feat: async call for current weather works for hard coded location "London, uk"

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -26,7 +26,7 @@ import Foundation
 
 struct WeatherInformation : Codable {
     let coord : WeatherCoord?
-    let weather : WeatherCond?
+    let weather : [WeatherCond]?
     let base : String?
     let main : WeatherMain?
     let visibility : Int?
@@ -95,3 +95,135 @@ struct WeatherSys : Codable {
 
 }
 
+struct WeatherErrorResponse: Codable {
+    
+    // {"cod":401, "message": "Invalid API key. Please see http://openweathermap.org/faq#error401 for more info."}
+    
+    // {"cod": 429,"message": "Your account is temporary blocked due to exceeding of requests limitation of your
+    //  subscription type. Please choose the proper subscription http://openweathermap.org/price" }
+    
+    let code: Int
+    let message: String
+    
+    enum CodingKeys: String, CodingKey {
+        case code
+        case message
+    }
+}
+
+extension WeatherErrorResponse: LocalizedError {
+    var errorDescription: String? {
+        return message
+    }
+}
+
+class OpenWeatherClient
+{
+    static let apiKey = ""
+    
+    enum Endpoints {
+        static let base = "https://api.openweathermap.org/data/2.5"
+        
+        case currentweather
+        
+        var stringValue : String {
+            switch self {
+            case .currentweather: return Endpoints.base + "/weather"
+            }
+        }
+        
+        var url: URL {
+            return URL(string: stringValue)!
+        }
+        
+    }
+    
+    class func taskForGETRequest<ResponseType: Decodable>(url: URL, queryItems: [URLQueryItem], responseType: ResponseType.Type, completion: @escaping (ResponseType?, Error?) -> Void) -> URLSessionDataTask {
+
+        var finalURL:URL = url;
+
+        var components = URLComponents(url: finalURL, resolvingAgainstBaseURL: false)!
+        
+        components.queryItems = queryItems
+        
+        if let urlWithQuery = components.url {
+            
+            finalURL = urlWithQuery
+        }
+        
+        print(finalURL)
+        
+        let request = URLRequest(url: finalURL)
+        // The default HTTP method for URLRequest is “GET”.
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data else {
+                DispatchQueue.main.async {
+                    completion(nil, error)
+                }
+                return
+            }
+            
+            //print(data.map { String(format: "%02x", $0) }.joined())
+            let string1 = String(data: data, encoding: String.Encoding.utf8) ?? "Data could not be printed"
+            print(string1)
+
+            let decoder = JSONDecoder()
+            do {
+                let responseObject = try decoder.decode(ResponseType.self, from: data)
+                DispatchQueue.main.async {
+                    completion(responseObject, nil)
+                }
+            } catch {
+                do {
+                    let errorResponse = try decoder.decode(WeatherErrorResponse.self, from: data) as Error
+                    DispatchQueue.main.async {
+                        completion(nil, errorResponse)
+                    }
+                } catch {
+                    DispatchQueue.main.async {
+                        completion(nil, error)
+                    }
+                }
+            }
+        }
+        task.resume()
+        
+        return task
+    }
+    
+    class func getCurrentWeather(completion: @escaping (Bool, Error?, WeatherInformation?) -> Void) {
+        
+        let qItems = [URLQueryItem(name: "q", value: "London,uk"),
+            URLQueryItem(name: "APPID", value: apiKey),
+            URLQueryItem(name: "units", value: "imperial")]
+        
+        //+ "?method=flickr.photos.search&format=json&nojsoncallback=1&api_key=" + restApiKey
+        
+        taskForGETRequest(url: Endpoints.currentweather.url, queryItems: qItems, responseType: WeatherInformation.self) { response, error in
+            if let response = response {
+                completion(true, nil, response)
+            } else {
+                print("false WeatherInformation")
+                completion(false, error, nil)
+            }
+        }
+    }
+    
+    class func handleCurrentWeatherResponse(success: Bool, error: Error?, response: WeatherInformation?) {
+        if success {
+            print("Happy WeatherInformation")
+            
+            if let thedata = response {
+                print("\(thedata)")
+            }
+            
+        } else {
+            let message = error?.localizedDescription ?? ""
+            print("\(message)");
+            print("Sad WeatherInformation")
+        }
+    }
+}
+
+OpenWeatherClient.getCurrentWeather(completion: OpenWeatherClient.handleCurrentWeatherResponse)
